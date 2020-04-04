ca0809081d04c32782abd6a24264e79f46f6033d
andrs99 <53583070+andrs99@users.noreply.github.com>
2020-04-04 00:03:53 -0600

    Add files via upload

---
+++ Biblioteca/Biblioteca/urls.py
@@ -0,0 +1,50 @@
+"""Biblioteca URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+
+from django.contrib.auth import login
+# from BusquedaLibros import views
+
+from Usuarios.views import *
+from BusquedaLibros.views import *
+
+
+
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/',busqueda_libros),
+    path('',busqueda_libros),
+    path('buscar/',buscar),
+    path('visualizar/',visualizar),
+    
+
+
+    path('register/', register),
+    path('login/', login),
+    path('logout/', logout),
+
+
+
+
+
+
+]
